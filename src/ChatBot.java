18787014180f03ef96e7d43d22ee92c6fb44b3a6
Anthony Sutandi <it1572010@student.it.maranatha.edu>
2018-04-09 17:31:59 +0700

    test

--- src/ChatBot.java
+++ src/ChatBot.java
@@ -1,4 +1,5 @@
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -9,32 +10,49 @@ public class ChatBot {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        String nama = s.next();
+        Random r = new Random();
+        int temp = r.nextInt(4);
         String buatberes = "selamat tinggal";
-        String buatberes2 = "dadah";
         String pertanyaan = "";
-        while (!pertanyaan.equals(buatberes) || !pertanyaan.equals(buatberes2)) {
-            System.out.print(nama+" : ");
-            pertanyaan=s.next();
-            String[] hasilsplit= pertanyaan.split(" ");
-            if(hasilsplit[0].toLowerCase().equals("siapa")){
-                
+        String[] daftarPertanyaan = new String[4];
+        daftarPertanyaan[0] = "Lagi ngapain ?";
+        daftarPertanyaan[1] = "Udah makan belom ?";
+        daftarPertanyaan[2] = "Kamu lagi dimana ?";
+        daftarPertanyaan[3] = "Gimana hari ini ?";
+//        daftarPertanyaan[4] = "";
+//        daftarPertanyaan[5] = "";
+//        daftarPertanyaan[6] = "";
+//        daftarPertanyaan[7] = "";
+//        daftarPertanyaan[8] = "";
+//        daftarPertanyaan[9] = "";
+        System.out.println("Regina : Hai, nama saya Regina, Siapa nama kamu ?");
+        String nama = s.nextLine();
+        nama = nama.trim();
+        String[] printnama = nama.split("\\s+");
+        if (nama.split(".").length == 1 || nama.split(",").length == 1) {
+            System.out.println("Regina : Hai " + printnama[printnama.length - 1] + " " + daftarPertanyaan[temp]);
+            pertanyaan = s.nextLine();
+        }
+
+        pertanyaan = nama;
+        while (!pertanyaan.toLowerCase().contains(buatberes)) {
+            String[] hasilsplit = pertanyaan.split(" ");
+            if (pertanyaan.toLowerCase().contains("siapa")) {
+                System.out.println("masuk ke siapa");
+            } else if (pertanyaan.toLowerCase().contains("kapan")) {
+                System.out.println("masuk ke kapan");
+            } else if (pertanyaan.toLowerCase().contains("dimana")) {
+                System.out.println("masuk ke dimana");
+            } else if (pertanyaan.toLowerCase().contains("mengapa") || pertanyaan.toLowerCase().contains("kenapa")) {
+                System.out.println("masuk ke mengapa");
+            } else if (pertanyaan.toLowerCase().contains("bagaimana")) {
+                System.out.println("masuk ke bagaimana");
+            } else if (pertanyaan.toLowerCase().contains("apa")) {
+                System.out.println("masuk ke apa");
+            } else {
+                System.out.println("Regina : Ga tau apa-apa");
             }
-            else if(hasilsplit[0].toLowerCase().equals("apa")){
-                
-            }
-            else if(hasilsplit[0].toLowerCase().equals("kapan")){
-                
-            }
-            else if(hasilsplit[0].toLowerCase().equals("dimana")){
-                
-            }
-            else if(hasilsplit[0].toLowerCase().equals("mengapa")){
-                
-            }
-            else if(hasilsplit[0].toLowerCase().equals("bagaimana")){
-                
-            }
+            pertanyaan = s.nextLine();
         }
     }
 }
